1b3532e0e3ede39a12f5913baf88880429088157
pradeep <pradeep>
2022-08-04 23:48:35 +0530

    add array destructuring

--- object-destructuring/script.js
+++ object-destructuring/script.js
@@ -1,1 +1,12 @@
 //object destructuring
+// object destructuring
+
+const bioData = {
+    name: "Pradeep",
+    age: 30,
+    deg: "MCA"
+}
+console.log(`my name is ${bioData.age}`);
+
+let {name, age, deg} = bioData;
+console.log(`my name is ${name}`);
